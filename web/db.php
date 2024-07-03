5ec68557925fb465e71a514955ea5e437a5f3c95
Wolfxzy15 <rej0715@gmail.com>
2024-07-03 17:50:31 +0800

    change db

--- web/db.php
+++ web/db.php
@@ -2,6 +2,6 @@
 $servername = "localhost"; // Change to your database server name
 $username = "root"; // Change to your database username
 $password = ""; // Change to your database password
-$dbname = "barangay"; // Change to your database name
+$dbname = "barangay1"; // Change to your database name
 
 $conn = new mysqli("$servername", $username, $password, $dbname);
