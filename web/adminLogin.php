643128f9d247390cc464d3485094aed481089391
Anton <aebinarao24@gmail.com>
2024-09-02 19:34:21 +0800

    changes adminLogin

--- web/adminLogin.php
+++ web/adminLogin.php
@@ -1,11 +1,9 @@
 <?php
 require 'adminFunction.php';
-if(isset($_SESSION["adminID"])){
-   header("Location: index.php");
+if (isset($_SESSION["adminID"])) {
+  header("Location: registerResident.php");
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,97 +11,62 @@ if(isset($_SESSION["adminID"])){
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Login Page</title>
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css2?family=Electrolize&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
   <style>
-   .center-image {
+    .center-image {
       display: flex;
       justify-content: center;
     }
-
-    .navbar-brand {
-      padding: 0;
-    }
-
-    #notification, #errorNotification {
-      display: none;
-      position: fixed;
-      left: 50%;
-      top: 12%;
-      transform: translate(-50%, -50%);
-      width: 80%;
-      text-align: center;
-      padding: 10px;
-      border-radius: 5px;
-      z-index: 1000;
-    }
-
-    #notification {
-      background-color: #28a745;
-      color: white;
-    }
-
-    #errorNotification {
-      background-color: #dc3545;
-      color: white;
-    }
-
   </style>
 </head>
-<body style="background-image: url('https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/e0/4c/2d/iloilo-city-hall.jpg?w=1200&h=-1&s=1');
-  background-repeat: no-repeat;
-  background-size: cover;">
- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">
-    <img src="https://www.clipartmax.com/png/middle/98-987300_seal-of-the-barangay-barangay-logo-png.png" 
-    width="85" height="70" class="d-inline-block align-center" alt="">
-  </a>
-  <h4 style="color: aliceblue;">Barangay</h4>
-    <!-- <a class="btn btn-dark ml-auto" href="register.php" role="button">ADMIN</a> --> 
-  </div>
-</nav><br>
-<div id="notification">Login Successful</div>
-<div id="errorNotification">Incorrect Username or Password</div>
 
-<div class="container mt-5">
-  <div class="row justify-content-center">
-    <div class="col-md-6">
-      <div class="card">
-        <div class="card-header">
-          <div class="center-image">
-          <img src="images/admin.png" 
-    width="90" height="90" alt="">
-
+<body>
+  <div class="container mt-5 pt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <div class="center-image">
+              <img src="images/citylogo.png"
+                width="90" height="90" alt="">
+            </div>
+            <h2 style="text-align: center;">Administrator Login</h2>
           </div>
-        
-          <h2 style="text-align: center;">Administrator Login</h2>
-        </div>  
-        <div class="card-body">
-        <form autocomplete="off" action="" method="post">
-        <input type="hidden" id="action" value="adminlogin">
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" class="form-control" id="username" placeholder="Admin Username" name="username" required>
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" class="form-control" id="password" placeholder="Admin Password" name="password" required>
-            </div>
-            <button type="button" onclick="submitData();" class="btn btn-dark">Login</button>
-            <p class="mb-0">Don't have an account? <a href="register.php">Register here</a>.</p>
-          </form>
-          
-          <?php require 'adminScript.php'; ?>
+          <div class="card-body">
+            <form autocomplete="off" action="" method="post">
+              <input type="hidden" id="action" value="adminlogin">
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Admin Username" name="username" required>
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Admin Password" name="password" required>
+              </div>
+              <button type="button" onclick="submitData();" class="btn btn-success" style="width: 100%">Login</button>
+              <br><br>
+              <p class="mb-0">Don't have an account? <a href="register.php">Register here</a>.</p>
+            </form>
+
+            <?php require 'adminScript.php'; ?>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 
-<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-</script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+  </script>
 
 
 </body>
-</html>
 
+</html>
